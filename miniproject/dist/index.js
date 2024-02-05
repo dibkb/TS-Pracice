31dae70e3e2223ced9eef525e9c25530bab6d3c5
dibkb <dibas9110@gmail.com>
2024-02-05 23:12:27 +0530

    todo

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("second");
+const button = document.getElementById("btn");
+const input = document.getElementById("input-text");
+const form = document.querySelector("form");
+const unorderedList = document.getElementById("ul");
+const addLi = (li) => {
+    unorderedList.append(li);
+};
+const formSubmitHandler = (e) => {
+    e.preventDefault();
+    const li = document.createElement("li");
+    if (input.value.length) {
+        li.innerHTML = input.value;
+        addLi(li);
+        input.value = "";
+    }
+};
+form.addEventListener("submit", formSubmitHandler);
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,1 +1,17 @@
-console.log("second");
+const button = document.getElementById("btn") as HTMLButtonElement;
+const input = document.getElementById("input-text")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const unorderedList = document.getElementById("ul")! as HTMLUListElement;
+const addLi = (li: HTMLLIElement) => {
+  unorderedList.append(li);
+};
+const formSubmitHandler = (e: SubmitEvent) => {
+  e.preventDefault();
+  const li = document.createElement("li");
+  if (input.value.length) {
+    li.innerHTML = input.value;
+    addLi(li);
+    input.value = "";
+  }
+};
+form.addEventListener("submit", formSubmitHandler);
